e610f7c7ce52576ad63a1748cfc55d968adb8444
Kelsie Braidwood <kelsieiona@gmail.com>
2018-05-17 13:45:23 +0100

    adds Runner set up & functionality as well as delete to DBHelper Class

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,6 +1,21 @@
+import db.DBHelper;
+import models.*;
+
+import java.util.GregorianCalendar;
+
 public class Runner {
 
     public static void main(String[] args) {
+
+        Day day = new Day(new GregorianCalendar(2018, 4, 23));
+        Meal meal1 = new Meal(MealType.LUNCH, day);
+        Food food1 = new Food("Pizza", 800.00, FoodType.CARBOHYDRATE, 9.99, meal1);
+
+        DBHelper.save(day);
+        DBHelper.save(meal1);
+        DBHelper.save(food1);
+
+
 
     }
 }
